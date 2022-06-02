7ade927ef0347cfbec95cf85e1b4910d87acdb6d
Maina Wycliffe <wmmaina7@gmail.com>
2022-06-02 15:24:03 +0300

    Convert to Standalone Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule, CommonModule],
 })
 export class AppComponent {
   title = 'angular-without-modules-google-io-extended-2022-demo';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from './app/app-routing.module';
+import { RouterModule } from '@angular/router';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(HttpClientModule), importProvidersFrom(RouterModule.forRoot([]))],
+}).catch(err => console.error(err));
