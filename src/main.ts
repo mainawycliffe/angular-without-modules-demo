0f524cf49e91feb27524ff0545494613200d3125
Maina Wycliffe <wmmaina7@gmail.com>
2022-06-03 12:55:04 +0300

    Remove AppRoutingModule

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,33 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { HttpClientModule } from "@angular/common/http";
-import { AppRoutingModule } from './app/app-routing.module';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewTodoComponent } from './app/view-todo/view-todo.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewTodoComponent,
+  },
+  {
+    // lazy loading a standalone component
+    path: 'add',
+    loadComponent: () =>
+      import('./app/add-todo/add-todo.component').then(
+        (c) => c.AddTodoComponent
+      ),
+    title: 'Add Todo',
+  },
+];
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule), importProvidersFrom(AppRoutingModule)],
-}).catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(RouterModule.forRoot(routes)),
+  ],
+}).catch((err) => console.error(err));
