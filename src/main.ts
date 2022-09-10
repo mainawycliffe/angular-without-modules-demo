5407b671ca7f496cc500d79540b5a6e4a2a94642
Maina Wycliffe <12270550+mainawycliffe@users.noreply.github.com>
2022-09-10 22:59:30 +0000

    Update to Angular 14.2.1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { RouterModule, Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 import { ViewTodoComponent } from './app/view-todo/view-todo.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {
@@ -26,5 +27,5 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+  providers: [provideRouter(routes), provideAnimations()],
 }).catch((err) => console.error(err));
